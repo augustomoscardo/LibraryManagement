f51f265c6ebfddf06524a1aea7a93ab9cd933b7a
Augusto Moscardo <augustomsaluz@gmail.com>
2026-05-11 08:15:32 -0300

    Add custom exception hierarchy for API error handling.

---
+++ LibraryManagement.Exceptions/ExceptionsBase/LibraryManagementException.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace LibraryManagement.Exceptions.ExceptionsBase;
+
+public abstract class LibraryManagementException : SystemException
+{
+    public LibraryManagementException(string errorMessage) : base(errorMessage)
+    {
+    }
+
+    public abstract List<string> GetErrors();
+    public abstract HttpStatusCode GetHttpStatusCode();
+}
---
+++ LibraryManagement.Exceptions/ExceptionsBase/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace LibraryManagement.Exceptions.ExceptionsBase;
+
+public class NotFoundException : LibraryManagementException
+{
+    public NotFoundException(string errorMessage) : base(errorMessage)
+    {}
+
+    public override List<string> GetErrors() => [Message];
+
+    public override HttpStatusCode GetHttpStatusCode() => HttpStatusCode.NotFound;
+}
